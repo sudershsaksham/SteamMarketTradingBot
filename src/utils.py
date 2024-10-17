95bf098eb218133f28c20fe7920ffdf9b0bb8306
Saksham Sudershan <sudershansaksham@gmail.com>
2024-10-17 01:12:28 -0400

    Final Code

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,6 +10,7 @@ import requests
 import time
 import pandas as pd
 from typing import List
+from datetime import datetime, timezone
 
 class Currency(Enum):
     """
@@ -55,7 +57,7 @@ class Currency(Enum):
     KWD = 38  # Kuwaiti dinar
     QAR = 39  # Qatari riyal
     CRC = 40  # Costa Rican colon
-
+    
 def scrape_item_names() -> List[str]:
     '''
     :returns:   List of all Dota 2 items from Steam Marketplace 
@@ -77,12 +79,19 @@ def scrape_item_names() -> List[str]:
         # Wait for page load
         time.sleep(10)
 
-        items = driver.find_elements(By.CSS_SELECTOR, '.market_listing_item_name_block')
-        
+        try:
+            items = driver.find_elements(By.CSS_SELECTOR, '.market_listing_item_name_block')
+        except:
+            not_last_page = False
+
         # Add item names to the set
         for item in items:
+            print(item.text.split('\n')[0])
             item_names.add(item.text.split('\n')[0])
             
+        if len(items) < 10:
+            break
+
         # Wait for NxtPage button to be clickable
         try:
             element = WebDriverWait(driver, 10).until(
@@ -118,43 +127,293 @@ def generate_price_history_url(item_name: str) -> str:
     url = base_url + process_name(item_name)
     return url
 
-def build_item_list() -> None:
+def generate_hero_id(item_name: str) -> str:
+    '''
+    :params:    Item Name
+    :returns:   Hero ID from Dota 2  
+    '''    
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Runs Chrome in headless mode
+    driver = webdriver.Chrome(options=chrome_options)
+
+    hero_dict = {
+        'Anti-Mage': 1,
+        'Axe': 2,
+        'Bane': 3,
+        'Bloodseeker': 4,
+        'Crystal Maiden': 5,
+        'Drow Ranger': 6,
+        'Earthshaker': 7,
+        'Juggernaut': 8,
+        'Mirana': 9,
+        'Shadow Fiend': 11,
+        'Morphling': 10,
+        'Phantom Lancer': 12,
+        'Puck': 13,
+        'Pudge': 14,
+        'Razor': 15,
+        'Sand King': 16,
+        'Storm Spirit': 17,
+        'Sven': 18,
+        'Tiny': 19,
+        'Vengeful Spirit': 20,
+        'Windranger': 21,
+        'Zeus': 22,
+        'Kunkka': 23,
+        'Lina': 25,
+        'Lich': 31,
+        'Lion': 26,
+        'Shadow Shaman': 27,
+        'Slardar': 28,
+        'Tidehunter': 29,
+        'Witch Doctor': 30,
+        'Riki': 32,
+        'Enigma': 33,
+        'Tinker': 34,
+        'Sniper': 35,
+        'Necrophos': 36,
+        'Warlock': 37,
+        'Beastmaster': 38,
+        'Queen of Pain': 39,
+        'Venomancer': 40,
+        'Faceless Void': 41,
+        'Skeleton King': 42,
+        'Death Prophet': 43,
+        'Phantom Assassin': 44,
+        'Pugna': 45,
+        'Templar Assassin': 46,
+        'Viper': 47,
+        'Luna': 48,
+        'Dragon Knight': 49,
+        'Dazzle': 50,
+        'Clockwerk': 51,
+        'Leshrac': 52,
+        "Nature's Prophet": 53,
+        'Lifestealer': 54,
+        'Dark Seer': 55,
+        'Clinkz': 56,
+        'Omniknight': 57,
+        'Enchantress': 58,
+        'Huskar': 59,
+        'Night Stalker': 60,
+        'Broodmother': 61,
+        'Bounty Hunter': 62,
+        'Weaver': 63,
+        'Jakiro': 64,
+        'Batrider': 65,
+        'Chen': 66,
+        'Spectre': 67,
+        'Doom': 69,
+        'Ancient Apparition': 68,
+        'Ursa': 70,
+        'Spirit Breaker': 71,
+        'Gyrocopter': 72,
+        'Alchemist': 73,
+        'Invoker': 74,
+        'Silencer': 75,
+        'Outworld Devourer': 76,
+        'Lycanthrope': 77,
+        'Brewmaster': 78,
+        'Shadow Demon': 79,
+        'Lone Druid': 80,
+        'Chaos Knight': 81,
+        'Meepo': 82,
+        'Treant Protector': 83,
+        'Ogre Magi': 84,
+        'Undying': 85,
+        'Rubick': 86,
+        'Disruptor': 87,
+        'Nyx Assassin': 88,
+        'Naga Siren': 89,
+        'Keeper of the Light': 90,
+        'Wisp': 91,
+        'Visage': 92,
+        'Slark': 93,
+        'Medusa': 94,
+        'Troll Warlord': 95,
+        'Centaur Warrunner': 96,
+        'Magnus': 97,
+        'Timbersaw': 98,
+        'Bristleback': 99,
+        'Tusk': 100,
+        'Skywrath Mage': 101,
+        'Abaddon': 102,
+        'Elder Titan': 103,
+        'Legion Commander': 104,
+        'Ember Spirit': 106,
+        'Earth Spirit': 107,
+        'Abyssal Underlord': 108,
+        'Terrorblade': 109,
+        'Phoenix': 110,
+        'Techies': 105,
+        'Oracle': 111,
+        'Winter Wyvern': 112,
+        'Arc Warden': 113,
+    }
+
+    url = "https://steamcommunity.com/market/listings/570/" + process_name(item_name)
+    driver.get(url)
+    try:
+        element = driver.find_element(By.CSS_SELECTOR, '.descriptor:nth-child(1)').text
+        if element.startswith("Used By: "):
+            hero_name = element.replace("Used By: ", "")
+            if hero_name in hero_dict:
+                return hero_dict[hero_name]
+            else:
+                print(f"{item_name} has no associated hero!")
+                return 0
+        else:
+            print(f"{item_name} has no associated hero!")
+            return 0
+    except:
+        return 0
+    
+def build_item_list(dir_path: str) -> None:
     '''
     Saves main.csv file with all item_name, hash_name and url
     '''
     item_names = scrape_item_names()
     hash_names = list()
     urls = list()
+    hero_ids = list()
 
     for item in item_names:
         hash_names.append(process_name(item))
         urls.append(generate_price_history_url(item))
+        hero_ids.append(generate_hero_id(item))
 
-    data = {'Item Name': item_names,
+    data = {
+        'Item Name': item_names,
         'Hash Name': hash_names,
-        'URL': urls}
+        'URL': urls,
+        'Hero ID': hero_ids
+        }
+    
     df = pd.DataFrame(data)
-    df.to_csv('data/main.csv', index=False)
+    df.to_csv(os.path.join(dir_path, 'main.csv'), index=False)
     print('Saved main.csv for all Dota 2 Immortal Standard Quality Items!')
 
-def scrape_item_price_history(url: str) -> dict:
+def scrape_item_price_history(url: str) -> requests.Response:
     '''
-    returns price history json
+    :params:    URL to use to ask for data
+    :returns:   Response Object
     '''
-    login_cookie_value = (
-        "76561198150110459%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MTAx"
-        "Ql8yNTMzNDUzMl8wRUVENCIsICJzdWIiOiAiNzY1NjExOTgxNTAxMTA0NTkiLCAiYXVkIjogWyAid2ViOmNvbW11"
-        "bml0eSIgXSwgImV4cCI6IDE3Mjg5Njg5MzcsICJuYmYiOiAxNzIwMjQxMDI3LCAiaWF0IjogMTcyODg4MTAyNywg"
-        "imp0aSI6ICIxMDFCXzI1MzM0NTMyXzExMDRCIiwgIm9hdCI6IDE3Mjg4ODEwMjcsICJydF9leHAiOiAxNzQ3MDYy"
-        "NTk3LCAicGVyIjogMCwgImlwX3N1YmplY3QiOiAiNC43MS4yNy4xNjEiLCAiaXBfY29uZmlybWVyIjogIjQuNzEu"
-        "MjcuMTYxIiB9.XzoBgqqapATy8nRMBCHbiO8HMI63FIFbII8oB7WSwDz63h470lbN5pdiJspMqEdyHvNjUUXISzMU"
-        "ygmeMW3VDw"
-    )
-    cookie = {'steamLoginSecure': login_cookie_value}    
-    data = requests.get(url, cookies=cookie)
-    return data.json()
+    # Open the file in read mode
+    with open('config.txt', 'r') as file:
+        login_cookie = file.read().split('\n')[0]
 
-def format_price_history_json():
+    header = {
+    'Cookie': f'steamLoginSecure={login_cookie}'
+    }
+    data = requests.get(url, headers=header)
+    return data
+
+def parse_price_history_json(item_name: str, data: dict, dir_path: str) -> pd.DataFrame:
     '''
-    takes json, cleans it up for backtest.py
+    :params:    Response Object for Item Price History Request
+                Item Name in Hash Format
+                Directory Path to Save Price History
+    :returns:   Pandas DF of Daily Prices for Item Name
     '''
+    if data.status_code != 200:
+        return False
+    else:
+        df = pd.DataFrame(data.json()['prices'])
+        df.rename(columns={0: 'Timestamp', 1: 'Price', 2: 'Volume'}, inplace=True)
+        df['Timestamp'] = df['Timestamp'].str.replace(r': \+\d{1}', '', regex=True)
+        df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%b %d %Y %H')
+        df['Price'] = df['Price'].astype('float')
+        df['Volume'] = df['Volume'].astype('int')
+        df.to_csv(os.path.join(dir_path, f'{item_name}.csv'), index = False)
+        return True
+
+def process_price_history_row(row: pd.Series, dir_path: str) -> bool:
+    '''
+    :params:    A Single Row of the pandas DF which contains item_name, hash_name and url columns
+    :saves:     A CSV file containing Price History
+    :returns:   True if request successful, False otherwise
+    '''
+    item_name = row['Hash Name']
+    if row['Hero ID'] == 0:
+        print(f'No price history for {row['Item Name']} as it is not a hero item')
+        return False
+    
+    data = scrape_item_price_history(row['URL'])
+    time.sleep(5)
+    if parse_price_history_json(item_name, data, dir_path):
+        return True
+    else:
+        print(f'Error: could not save price history for {row['Item Name']}')
+        return False
+
+def convert_unix_to_date(unix_timestamp):
+    return datetime.fromtimestamp(unix_timestamp, tz=timezone.utc).strftime('%Y-%m-%d')
+
+def send_graphql_query(url, query, variables=None, headers=None):
+    """Sends a GraphQL query and returns the response."""
+    json_data = {
+        'query': query,
+        'variables': variables or {}
+    }
+    
+    response = requests.post(url, json=json_data, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Query failed with status code {response.status_code}: {response.text}")
+
+def get_hero_stats(hero_id, till_date, token):
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "User-Agent": "STRATZ_API",
+    }
+    endpoint = "https://api.stratz.com/graphql"
+    
+    query = '''
+    query ($heroId: [Short]!, $skip: Int!) {
+        heroStats {
+            winMonth(heroIds: $heroId, skip: $skip) {
+                month
+                matchCount
+                winCount
+            }
+        }
+    }
+    '''
+    
+    # Initialize variables
+    skip = 0
+    batch_size = 8
+    all_results = []  # List to store all fetched data
+    
+    while True:
+        time.sleep(2)
+        variables = {
+            "heroId": hero_id,
+            "skip": skip
+        }
+        
+        # Send the GraphQL query
+        response = send_graphql_query(endpoint, query, variables, headers)
+        data = response.get('data', {}).get('heroStats', {}).get('winMonth', [])
+        for each_row in data:
+            each_row['month'] = convert_unix_to_date(each_row['month'])
+
+        # Break the loop if no more data is returned
+        if not data:
+            break
+        
+        # Accumulate results
+        all_results.extend(data)
+        
+        # Increment skip for pagination
+        skip += batch_size
+        
+        # Check if we've reached the desired date and stop if necessary
+        last_day = data[-1]['month']
+        if last_day < till_date:
+            break  # Stop fetching if we have reached or passed the till_date
+    
+    # Process or save the accumulated results as needed
+    return all_results
